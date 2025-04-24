3f930e2d29bb20d0bb4536ef18b633fc6877a118
Amin Ghiyati <aghiyatiiz@gmail.com>
2025-04-24 11:24:09 +0200

    Stand: Lerneinheit10-1 abgeschlossen

--- src/userInterface/PaintPanel.java
+++ src/userInterface/PaintPanel.java
@@ -5,32 +5,54 @@ import labTasks.Desk;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class PaintPanel extends JPanel {
     private static final long serialVersionUID = 1L;
 
+    private ArrayList<Chair> chairList;
+    private ArrayList<Desk> deskList;
+
+    public PaintPanel() {
+        chairList = new ArrayList<>();
+        deskList = new ArrayList<>();
+
+        int numChairs = 2;
+        int numDesks = 2;
+
+        Random rand = new Random();
+
+
+        for (int i = 0; i < numChairs; i++) {
+            Chair chair = new Chair(Color.BLUE);
+            chair.setX(rand.nextInt(551)); // 0 bis 550
+            chair.setY(rand.nextInt(451)); // 0 bis 450
+            chairList.add(chair);
+        }
+
+
+        for (int i = 0; i < numDesks; i++) {
+            Desk desk = new Desk(Color.RED);
+            desk.setX(rand.nextInt(551));
+            desk.setY(rand.nextInt(451));
+            deskList.add(desk);
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D graphic2d = (Graphics2D) g;
+        Graphics2D g2d = (Graphics2D) g;
 
 
-        Desk desk = new Desk(Color.RED);
-        desk.setX(240);
-        desk.setY(270);
-
-        Chair chair1 = new Chair(Color.BLUE);
-        chair1.setX(50);
-        chair1.setY(200);
-
-        Chair chair2 = new Chair(Color.BLUE);
-        chair2.setX(350);
-        chair2.setY(200);
+        for (Chair c : chairList) {
+            c.paintChair(g2d);
+        }
 
 
-        desk.paintDesk(graphic2d);
-        chair1.paintChair(graphic2d);
-        chair2.paintChair(graphic2d);
-
+        for (Desk d : deskList) {
+            d.paintDesk(g2d);
+        }
     }
 }
